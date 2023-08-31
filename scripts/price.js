ebdf05de731466403541b5b01dc872bafcd01e62
nancyhalladay24 <nancy.halladay1@nhs.net>
2023-08-31 16:26:08 +0100

    first commit

---
+++ scripts/price.js
@@ -0,0 +1,16 @@
+const setAmount = 2978.0; // The fixed set amount
+const outputElement = document.getElementById('output'); // Get the output element
+
+// Get all elements with the class "price"
+const priceElements = document.querySelectorAll('.Price');
+
+// Loop through each price element and calculate the percentage
+priceElements.forEach((priceElements) => {
+  const priceText = priceElements.textContent;
+  const priceValue = parseFloat(priceText.replace('£', '')); // Convert the price text to a numeric value
+  const percentage = (priceValue / setAmount) * 100;
+
+  // Create a message and update the content of the output element
+  const message = `£{priceText} is £{percentage}% of £{setAmount}<br>`;
+  outputElement.innerHTML += message; // Append the message to the existing content
+});
